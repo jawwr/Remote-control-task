d25de97459afbf0038d7011e6b4b59bddce8d700
Arseny <20mineslend14@gmail.com>
2022-10-20 15:08:01 +0500

    task

---
+++ src/main/java/Server.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+
+    public static void main(String[] args) {
+        try {
+            try {
+                serverSocket = new ServerSocket(1111);
+                System.out.println("Server was started");
+                clientSocket = serverSocket.accept();
+
+                try {
+                    while (true) {
+                        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                        String word = in.readLine();
+                        if (word.equals(":q")) {
+                            out.write("Connection closed by foreign host.");
+                            break;
+                        }
+                        String answer = execCommand(word);
+                        out.write(answer);
+                        out.write('\n');
+                        out.flush();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    in.close();
+                    out.close();
+                    clientSocket.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                serverSocket.close();
+                System.out.println("Server was stopped");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String execCommand(String command) {
+        String[] commands = command.split(" ");
+        ProcessBuilder process = new ProcessBuilder()
+                .command("/bin/bash", "-c")
+                .command(commands)
+                .redirectErrorStream(true);
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.start().getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+
+        } catch (Exception e) {
+            sb.delete(0, sb.length());
+            sb.append("Error");
+            e.printStackTrace();
+        }
+        sb.delete(sb.length() - 1, sb.length());
+        return sb.toString();
+    }
+}
